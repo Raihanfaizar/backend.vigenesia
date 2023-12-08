1a67418db5f3291366c8a96a84b0b25f9461e7f2
rizkyadh <rizkyadha210@gmail.com>
2023-12-08 22:52:48 +0700

    update routes and add filter

--- app/Config/Routes.php
+++ app/Config/Routes.php
@@ -6,3 +6,34 @@ use CodeIgniter\Router\RouteCollection;
  * @var RouteCollection $routes
  */
 $routes->get('/', 'Home::index');
+
+$routes->group('api', static function ($routes) {
+  $routes->resource('categories', [
+    'controller' => API\CategoriesController::class,
+    'filter' => \App\Filters\AuthenticationFilter::class,
+  ]);
+  $routes->resource('posts', [
+    'controller' => API\PostsController::class,
+    'filter' => \App\Filters\AuthenticationFilter::class,
+  ]);
+  $routes->resource('reports', [
+    'controller' => API\ReportsController::class,
+    'filter' => \App\Filters\AuthenticationFilter::class,
+  ]);
+  $routes->resource('roles', [
+    'controller' => API\RolesController::class,
+    'filter' => \App\Filters\AuthenticationFilter::class,
+  ]);
+  $routes->resource('users', [
+    'controller' => API\UsersController::class,
+    'filter' => \App\Filters\AuthenticationFilter::class,
+  ]);
+  $routes->post('login', [
+    \App\Controllers\API\UsersController::class,
+    'login'
+  ]);
+  $routes->post('register', [
+    \App\Controllers\API\UsersController::class,
+    'create'
+  ]);
+});
